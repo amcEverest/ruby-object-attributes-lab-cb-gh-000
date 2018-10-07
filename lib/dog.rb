48cb162120993ad4e954eb698f489f049a608314
Andres Cortez <andres.corteztm@gmail.com>
2018-10-07 19:26:57 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,17 +8,7 @@ class Dog
     @name
   end
 
-  def name (new_name)
-    @name= new_name
-    @name
-  end
-
   def breed
-    @breed
-  end
-
-  def breed (new_breed)
-    @breed= new_breed
     @breed
   end
 end
