a33fb0518cb13e3e8fa20ac07db7810b1115585d
Andres Cortez <andres.corteztm@gmail.com>
2018-10-07 19:29:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,6 @@
 class Dog
   def initialization(name,breed)
-    @name== name
-    @breed== breed
-  end
-
-  def name
-    @name
-  end
-
-  def breed
-    @breed
+    @name= name
+    @breed= breed
   end
 end
